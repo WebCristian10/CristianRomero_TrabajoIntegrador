9dc581dbb38139027980c167c153bb788555ed0c
cristianms1009 <cristian1009.argentinaprograma@gmail.com>
2022-09-21 11:09:23 -0300

    Creamos Backend

---
+++ Integrador/BackEnd/src/main/java/com/portfolio/CGR/CgrApplication.java
@@ -0,0 +1,13 @@
+package com.portfolio.CGR;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CgrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CgrApplication.class, args);
+	}
+
+}
---
+++ Integrador/BackEnd/src/test/java/com/portfolio/CGR/CgrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.portfolio.CGR;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CgrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
